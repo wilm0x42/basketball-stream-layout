69b963b979a4de21981d5c59d2eb1c8fa999fce6
wilm0x42 <wilm0x42@startmail.com>
2018-01-15 21:54:20 -0500

    Basic functionality of team names

--- extension.js
+++ extension.js
@@ -1,5 +1,21 @@
 'use strict';
 
-module.exports = function (nodecg) {
+module.exports = function (nodecg)
+{
+	var homeName;
+	var awayName;
 
+	nodecg.listenFor('updateHomeName', (value) =>
+	{
+    	homeName = value;
+	});
+	nodecg.listenFor('updateAwayName', (value) =>
+	{
+    	awayName = value;
+	});
+
+	nodecg.listenFor('getTeamNames', (error, ack) =>
+	{
+		ack(null, homeName, awayName);
+	});
 };
